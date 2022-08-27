65b61ad0f31347eee8504d77f000a5b70b8fb0fa
Icosider <iura.andrianov@Yandex.ru>
2022-08-28 01:33:20 +0300

    Добавлен пример для статьи "Добавление рецепта"

--- src/main/java/ru/mcmodding/tutorial/common/CommonProxy.java
+++ src/main/java/ru/mcmodding/tutorial/common/CommonProxy.java
@@ -6,6 +6,9 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import ru.mcmodding.tutorial.McModding;
 import ru.mcmodding.tutorial.common.handler.*;
@@ -32,6 +35,12 @@ public class CommonProxy {
     }
 
     public void postInit(FMLPostInitializationEvent event) {
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.RUBY_SWORD), " R ", " R ", " S ", 'R', ModItems.RUBY, 'S', Items.stick);
 
+        for (int damage = 0; damage < 15; damage++) {
+            GameRegistry.addShapelessRecipe(new ItemStack(ModItems.BALLOON, 1, damage), new ItemStack(Blocks.wool, 1, ~damage & 15), Items.string);
+        }
+
+        GameRegistry.addSmelting(ModBlocks.RUBY_ORE, new ItemStack(ModItems.RUBY), 5F);
     }
 }
